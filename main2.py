806e2bf50689aca2585648c9a18895e86fc2c218
Rose-mkt <rose.mkt@gmail.com>
2024-08-21 16:09:43 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -11,6 +11,15 @@ df
 x = 4*np.random.rand(100)
 y = np.sin(2*x + 1) + 0.1* np.random.randn(100)
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+st.sidebar.title('Classification')
+classifier = st.sidebar.selectbox('Select classifier', ('KNN', 'DT', 'RF', 'SVM', 'NN'))
+k = st.sidebar.slider('K', 1, 120, 3)
+from sklearn.metrics import accuracy_score
+y_pred = knn.predict(x_test)
+accuracy_score(y_test, y_pred)
+
 fig, ax = plt.subplots()
 ax.scatter(x, y)
 ax.scatter(x, y_pred)
