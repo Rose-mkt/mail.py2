1d369cb2238c6220f0e83efa28fbcd54094fb3da
Rose-mkt <rose.mkt@gmail.com>
2024-08-21 16:08:06 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -8,32 +8,11 @@ from sklearn.svm import SVC
 df = sns.load_dataset('iris')
 df
 
-x = df.iloc[:, :-1]
-y = df['species']
+x = 4*np.random.rand(100)
+y = np.sin(2*x + 1) + 0.1* np.random.randn(100)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+ax.scatter(x, y_pred)
+st.pyplot(fig)
 
-st.sidebar.title('Classification')
-classifier = st.sidebar.selectbox('Select classifier', ('KNN', 'DT', 'RF', 'SVM', 'NN'))
-k = st.sidebar.slider('K', 1, 120, 3)
-from sklearn.metrics import accuracy_score
-y_pred = knn.predict(x_test)
-accuracy_score(y_test, y_pred)
-
-from sklearn.svm import SVC
-svm = SVC()
-svm.fit(x_train, y_train)
-y_pred = svm.predict(x_test)
-accuracy_score(y_test, y_pred)
-
-from sklearn.tree import DecisionTreeClassifier
-dt = DecisionTreeClassifier()
-dt.fit(x_train, y_train)
-y_pred = dt.predict(x_test)
-accuracy_score(y_test, y_pred)
-
-from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier()
-rf.fit(x_train, y_train)
-y_pred = rf.predict(x_test)
-accuracy_score(y_test, y_pred)
